c5138419235d1a3a6ed285846699d952de5481ff
Michael George <georgem@wlwv.k12.or.us>
2020-03-06 09:24:54 -0800

    commit

--- Square.java
+++ Square.java
@@ -1,3 +1,13 @@
 public class Square extends Rectangle{
-    public Square()
+    public Square(int s1){
+        super(s, s);
+    }
+
+    public String toString(){
+        return super.toString() + "[Square]";
+        Quadrilateral[] shapes = (new Rectangle(4, 8), new Square(s: 5));
+        System.out.println(shapes[0].toString);
+        System.out.println(shapes[1].toString);
+        
+    }
 }
