37875eb88834898958fbc3d3690f5df0ab3afc7f
Ryans63 <54951373+Ryans63@users.noreply.github.com>
2020-03-30 12:31:25 -0700

    Update Lettuce.java

--- Lettuce.java
+++ Lettuce.java
@@ -1,5 +1,19 @@
 public class Lettuce extends Vegetable{
 
-
+  public Lettuce(){
+    super("Lettuce", "Leafy Greens");
+  }   
+  
+  public String feed(){
+    return "No need to feed it"; 
+  } 
+  
+   public String water(){
+    return "Water it everyday"; 
+  } 
+  
+   public String harvest(){
+    return "Harvest after 8 weeks of planting"; 
+  } 
 
 }
