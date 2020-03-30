dfe960a1d626dbcbc8cb082681a7f8deb0807e24
Ryans63 <54951373+Ryans63@users.noreply.github.com>
2020-03-30 12:25:22 -0700

    Update Cabbage.java

--- Cabbage.java
+++ Cabbage.java
@@ -1,3 +1,19 @@
 public class Cabbage extends Vegetable{
-
+  
+  public Cabbage(){
+    super("Cabbage", "Cruciferous");
+  }   
+  
+  public String feed(){
+    return "No need to feed it"; 
+  } 
+  
+   public String water(){
+    return "Water it once a week"; 
+  } 
+  
+   public String harvest(){
+    return "Harvest after 3 months of plant"; 
+  } 
+  
 }
