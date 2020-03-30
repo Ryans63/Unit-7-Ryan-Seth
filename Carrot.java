28939a395668daa108047ecb51be1dadb63a9168
Ryans63 <54951373+Ryans63@users.noreply.github.com>
2020-03-30 12:29:06 -0700

    Update Carrot.java

--- Carrot.java
+++ Carrot.java
@@ -1,5 +1,19 @@
 public class Carrot extends Vegetable{
 
-
+public Carrot(){
+    super("Carrot", "Root Vegetable");
+  }   
+  
+  public String feed(){
+    return "No need to feed it"; 
+  } 
+  
+   public String water(){
+    return "Water it once a day"; 
+  } 
+  
+   public String harvest(){
+    return "Harvest after 2-4 months of planting"; 
+  } 
 
 }
